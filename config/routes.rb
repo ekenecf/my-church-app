3ae21782b1a59012ede393b1db8174586a04bbef
ekenecf <ekens2u@gmail.com>
2022-09-07 16:39:43 +0100

    Create events

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
   resources :users
   resources :members, only: %i[index show]
   resources :groups, only: %i[index show]
+  resources :events, only: %i[index show]
 
   resources :users do
     resources :members, only: %i[index new show create destroy update]
     resources :groups, only: %i[index new show create destroy update]
+    resources :events, only: %i[index new show create destroy update]
   end
 
   post '/auth/login', to: 'authentication#login'
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_07_095935) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_07_125448) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.string "images"
+    t.text "description"
+    t.string "date"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
+  create_table "group_members", force: :cascade do |t|
+    t.bigint "group_id"
+    t.bigint "member_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_group_members_on_group_id"
+    t.index ["member_id"], name: "index_group_members_on_member_id"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "detail"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
 
   create_table "members", force: :cascade do |t|
     t.string "name"
@@ -37,5 +66,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_07_095935) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "events", "users"
+  add_foreign_key "group_members", "groups"
+  add_foreign_key "group_members", "members"
+  add_foreign_key "groups", "users"
   add_foreign_key "members", "users"
 end
