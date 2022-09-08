12904c67bff13adbc598ef3fd7229578ad6ff35e
ekenecf <ekens2u@gmail.com>
2022-09-08 11:49:58 +0100

    Make changes on the controllers

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,12 +1,15 @@
 class MembersController < ApplicationController
+  skip_before_action :authenticate_request
   before_action :set_member_params, only: %i[show update destroy]
 
   def index
-    @user = User.find(params[:user_id])
-    @members = Member.includes(:users).where(user: @user.id).order(name: :asc)
+    @members = Member.all
+    render json: @members, status: :ok
   end
 
-  def show; end
+  def show
+    render json: @member, status: :ok
+ end
 
   def new
     @user = User.find(params[:user_id])
@@ -16,24 +19,24 @@ class MembersController < ApplicationController
 
   def create
     member_p = member_params
-    created_member = Member.new(name: member_p[:name], phone_number: member_p[:phone_number],
+    @created_member = Member.new(name: member_p[:name], phone_number: member_p[:phone_number],
                                 occupation: member_p[:occupation], picture: member_p[:picture],
                                 distance: member_p[:distance], active: member_p[:active],
                                 post_held: member_p[:post_held], birthday: member_p[:birthday])
     @user = User.find(params[:user_id])
-    created_member.user = @user
-    created_member.user_id = @user.id
+    @created_member.user = @user
+    @created_member.user_id = @user.id
 
-    if created_member.save
+    if @created_member.save
       @group = Group.find(member_p[:group_id])
-      created_member.groups << @group
-      render json: created_member, status: :created
+      @created_member.groups << @group
+      render json: @created_member, status: :created
       flash[:notice] = 'member created successfully.'
-      redirect_to user_members_path(@user, created_member)
+      redirect_to user_members_path(@user, @created_member)
     else
-      render json: { errors: created_member.errors.full_messages },
+      render json: { errors: @created_member.errors.full_messages },
              status: :unprocessible_entity
-      @member = created_member
+      @member = @created_member
       render :new
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  include JsonWebToken
   skip_before_action :authenticate_request, only: [:create]
   before_action :set_user, only: %i[show destroy update]
 
